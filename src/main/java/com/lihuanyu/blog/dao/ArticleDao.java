98d7afd0cd753edf4d8c8a201a9ba9cee891a468
skyADMIN <546485299@qq.com>
2016-07-10 13:23:03 +0800

    dao到专门的包里，加入security依赖

--- src/main/java/com/lihuanyu/blog/dao/ArticleDao.java
+++ src/main/java/com/lihuanyu/blog/dao/ArticleDao.java
@@ -1,9 +1,12 @@
-package com.lihuanyu.blog.model;
+package com.lihuanyu.blog.dao;
 
+import com.lihuanyu.blog.model.Article;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by skyADMIN on 16/6/19.
  */
 public interface ArticleDao extends CrudRepository<Article, Integer> {
+    public Iterable<Article> findAllByOrderById();
 }
